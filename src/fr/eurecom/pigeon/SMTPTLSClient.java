a7f7fb1551af2efb6b8c774a4c3d5c32a0a67df8
Giuseppe Rizzo <giuse.rizzo@gmail.com>
2012-12-19 19:31:28 +0100

    SMTPTLCClient implements ISMTPClient

--- src/fr/eurecom/pigeon/SMTPTLSClient.java
+++ src/fr/eurecom/pigeon/SMTPTLSClient.java
@@ -1,4 +1,4 @@
-//   pigeon - Library for sending emails using SSL and TLS connection
+//   pigeon - Library for sending emails using both SSL/TLS
 //
 //   Copyright 2012 held by the author
 //
@@ -14,23 +14,38 @@
 package fr.eurecom.pigeon;
 
 import java.security.Security;
+import java.util.Date;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 
-public class SMTPTLSClient {
+public class SMTPTLSClient implements ISMTPClient {
 
-    public static boolean send( final String username, final String password, 
-                                String server, String from, String to, String cc,
-                                String bcc, String subject, String text, 
-                                boolean validateCertificate ) 
+    public boolean send(
+                            final String username, 
+                            final String password, 
+                            String server, 
+                            String from, 
+                            String to, 
+                            String cc,
+                            String bcc, 
+                            String subject, 
+                            String text, 
+                            String filename,
+                            Boolean validateCertificate 
+                        ) 
     {
 
         if (!validateCertificate)
@@ -52,21 +67,46 @@ public class SMTPTLSClient {
           });
  
         try { 
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(from));
-                       
+            MimeMessage message = new MimeMessage(session);
+            message.setSentDate(new Date());
+            message.setHeader("X-Mailer", "pigeon-v0.1");
+            
+            message.setFrom(new InternetAddress(from));                       
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
             if(cc != null) message.setRecipients(Message.RecipientType.CC, InternetAddress.parse(cc));
             if(bcc != null) message.setRecipients(Message.RecipientType.BCC, InternetAddress.parse(bcc));
-            
             message.setSubject(subject);
-            message.setText(text);
+            
+            if(filename == null)
+                message.setText(text, "utf-8");
+            
+            else {
+                // create and fill the first message part
+                MimeBodyPart body = new MimeBodyPart();
+                body.setText(text, "utf-8");
+    
+                // create the second message part
+                MimeBodyPart attachment = new MimeBodyPart();
+                // attach the file to the message
+                FileDataSource fds = new FileDataSource(filename);
+                attachment.setDataHandler(new DataHandler(fds));
+                attachment.setFileName(fds.getName());
+    
+                // create the Multipart and add its parts to it
+                Multipart mp = new MimeMultipart();
+                mp.addBodyPart(body);
+                mp.addBodyPart(attachment);
+    
+                // add the Multipart to the message
+                message.setContent(mp);
+            }
+                        
             Transport.send(message);
  
         } catch (MessagingException e) {
+            e.printStackTrace();
             throw new RuntimeException(e);
         }
         return true;
     }
-
 }
